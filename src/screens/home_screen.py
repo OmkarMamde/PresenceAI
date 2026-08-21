5cebf047587957c532cbbdb08db9b9bf0dd10c23
Omkar Mamde <mamdeomkar23@gmail.com>
2026-08-21 15:23:44 +0530

    student teacher png edit

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -2,6 +2,7 @@ import streamlit as st
 from src.components.header import header_home
 from src.ui.base_layout import style_base_layout,style_background_home
 from src.components.footer import footer_home
+from pathlib import Path
 # import base64
 
 # def get_base64(path):
@@ -15,12 +16,12 @@ def home_screen():
     style_background_home()
     style_base_layout()
     
-    
+    BASE_DIR = Path(__file__).resolve().parents[2]
     col1, col2=st.columns(2,gap="large")
 
     with col1:
         st.header("I'm Student")
-        st.image(".\images\mascot-student.png", width=120)
+        st.image(BASE_DIR / "images" / "mascot-student.png", width=120)
         # st.image("https://i.ibb.co/844D9Lrt/mascot-teacher.png",width=120)
         if st.button("Student Portal", type="primary", icon=':material/arrow_outward:',icon_position="right"):
             st.session_state["login_type"]="student"
@@ -28,7 +29,7 @@ def home_screen():
             
     with col2:
         st.header("I'm Teacher")
-        st.image(".\\images\\mascot-prof.png",width=150)
+        st.image(BASE_DIR / "images" / "mascot-teacher.png",width=150)
         # st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png",width=145)
         if st.button("Teacher Portal",type="tertiary", icon=':material/arrow_outward:', icon_position='right'):
             st.session_state["login_type"]="teacher"
